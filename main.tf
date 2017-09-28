5fd9422c7a3f1c280e7187ad5ebae4108fd20ded
Jakub Scholz <www@scholzj.com>
2017-09-28 20:45:55 +0200

    Use the kubernetes module

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "kubernetes" {
+  source = "scholzj/kubernetes/aws"
+
+  aws_region            = "${var.aws_region}"
+  cluster_name          = "${var.cluster_name}"
+  master_instance_type  = "${var.master_instance_type}"
+  worker_instance_type  = "${var.worker_instance_type}"
+  ssh_public_key        = "${var.ssh_public_key}"
+  master_subnet_id      = "${var.master_subnet_id}"
+  worker_subnet_ids     = "${var.worker_subnet_ids}"
+  min_worker_count      = "${var.min_worker_count}"
+  max_worker_count      = "${var.max_worker_count}"
+  hosted_zone           = "${var.hosted_zone}"
+  hosted_zone_private   = "${var.hosted_zone_private}"
+  tags                  = "${var.tags}"
+  tags2                 = "${var.tags2}"
+  addons                = "${var.addons}"
+  ssh_access_cidr       = "${var.ssh_access_cidr}"
+  api_access_cidr       = "${var.api_access_cidr}"
+}
---
+++ outputs.tf
@@ -0,0 +1,23 @@
+#####
+# Output
+#####
+
+output "minikube_dns" {
+    description = "DNS hostname of the Minikube"
+    value = "${module.kubernetes.dns}"
+}
+
+output "minikube_ip" {
+    description = "IP address of the Minikube"
+    value = "${module.kubernetes.public_ip}"
+}
+
+output "copy_config_dns" {
+    description = "Command for copying the kubeconfig for connecting using DNS address"
+    value = "To copy the kubectl config file using DNS record, run: 'scp ${module.kubernetes.ssh_user}@${module.kubernetes.dns}:${module.kubernetes.kubeconfig_dns} .'"
+}
+
+output "copy_config_ip" {
+    description = "Command for copying the kubeconfig for connecting using IP address"
+    value = "To copy the kubectl config file using IP address, run: 'scp ${module.kubernetes.ssh_user}@${module.kubernetes.public_ip}:${module.kubernetes.kubeconfig_ip} .'"
+}
